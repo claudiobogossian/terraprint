cddbac093af39293b2f8340a2b1825d13639d20e
Mário Rocco Pettinati <mario@funcate.org.br>
2015-11-23 10:23:54 -0200

    A small fix that changes the order that the pageSize is defined when a template is imported

--- src/terralib/layout/qt/core/View.cpp
+++ src/terralib/layout/qt/core/View.cpp
@@ -1168,12 +1168,6 @@ bool te::layout::View::importTemplate( EnumType* type )
     return false;
 
   bool result = scne->buildTemplate(m_visualizationArea, type, j_name);
-  if(result)
-  {
-    m_visualizationArea->build();
-    onChangeConfig();
-  }
-
   return result;
 }
 
