50847c91c648a867f0d80a3aa5f49fd4b31e6e24
Ricardo <ricardo.bonfiglioli@funcate.org.br>
2016-02-05 15:51:46 -0200

    Adds tranlation file of plugin (fixes #124), renames qt translation file to correct local language and adds the settings to install them

---
+++ share/terralib/translations/terralib_mod_qt_plugins_layout_pt_br.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>te::qt::plugins::layout::LayoutEditorAction</name>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/layout/LayoutEditorAction.cpp" line="61"/>
+        <location filename="../../../src/terralib/qt/plugins/layout/LayoutEditorAction.cpp" line="157"/>
+        <source>Layout Editor...</source>
+        <translation>Editor de Cartas...</translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/layout/LayoutEditorAction.cpp" line="203"/>
+        <source>Exit</source>
+        <translation>Sair</translation>
+    </message>
+</context>
+<context>
+    <name>te::qt::plugins::layout::Plugin</name>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/layout/Plugin.cpp" line="85"/>
+        <location filename="../../../src/terralib/qt/plugins/layout/Plugin.cpp" line="119"/>
+        <source>TerraPrint</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
