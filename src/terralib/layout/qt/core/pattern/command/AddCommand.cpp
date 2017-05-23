fa18cad9046ad494b7042490f2b7a32dc9ee6f7d
juliana.coelho <juliana.coelho@funcate.org.br>
2017-05-23 10:02:41 -0300

    Fix infinity looping in Line Item remove: Changes in the bounding rect or geometry of the line component should call the prepareGeometryChange() method at the time of changing the properties, so that the boxes tree of the scene is correctly synchronized and does not give problems in the removal.

--- src/terralib/layout/qt/core/pattern/command/AddCommand.cpp
+++ src/terralib/layout/qt/core/pattern/command/AddCommand.cpp
@@ -70,16 +70,10 @@ void te::layout::AddCommand::undo()
   AbstractItemView* abstractItem = dynamic_cast<AbstractItemView*>(m_item);
   if (!abstractItem)
     return;
-
-  const Property& pName = abstractItem->getProperty("name");
-  std::string nameItem = te::layout::Property::GetValueAs<std::string>(pName);
-
-  m_scene->removeItem(m_item);
-    
-  if (m_item->scene() != scene)
-    return;
-
+  
+  m_scene->removeItem(m_item);    
   scene->addItemStackWithoutScene(m_item);
+  m_scene->clearSelection();
   m_scene->update();
 }
 
